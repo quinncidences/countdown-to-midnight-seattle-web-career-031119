7eb64a9b7054bfb54d33f678da02654387f320b1
Quinn Cox <qrc.inpa@gmail.com>
2019-02-26 02:16:31 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,10 +1,16 @@
-#write your code here
-x=10
-def countdown(x)
-  while x > 0
-    puts "#{x} SECOND(S)!"
-    x -= 1
+def countdown(seconds)
+  while seconds > 0
+    puts "#{seconds} SECOND(S)!"
+    seconds -= 1
   end
+  return "HAPPY NEW YEAR!"
 end
+
+def countdown_with_sleep(seconds)
+  while seconds > 0
+    puts "#{seconds} SECOND(S)!"
+    seconds -= 1
+    sleep(1)
+  end
   return "HAPPY NEW YEAR!"
 end
